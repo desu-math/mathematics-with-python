1f92620766227c8659a22418613309871225ae64
Desalu Horsa <desaluhorsa83@gmail.com>
2026-07-02 13:15:10 +0300

    Add truncation error calculation for Taylor series

---
+++ numerical-analysis/01-error-analysis/error/truncation-error/example-1.py
@@ -0,0 +1,25 @@
+# truncation error for taylor series of f(x) = e^x
+import math
+
+# Input
+x = float(input("Enter x: "))
+n = int(input("Enter the number of Taylor series terms: "))
+
+# Taylor series approximation
+approximation = 0
+
+for k in range(n):
+    approximation += (x ** k) / math.factorial(k)
+
+# Exact value
+exact = math.exp(x)
+
+# Truncation error
+truncation_error = abs(exact - approximation)
+
+# Output
+print("\nResult")
+print("------")
+print(f"Exact value         = {exact}")
+print(f"Approximate value   = {approximation}")
+print(f"Truncation error    = {truncation_error}")
